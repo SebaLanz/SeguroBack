d394479bde34e6571cf60bea7b532ebc997ddec5
marcos <marcosdeluca@outlook.com>
2022-09-15 19:27:52 -0300

    delete, insert creados

--- clases/producto.class.php
+++ clases/producto.class.php
@@ -26,8 +26,57 @@ class Producto extends BaseBiz{
             throw new Exception(" Error obteniendo producto : ".$e->getMessage());         
         }
     }
+public function save($id_producto = 0, $producto, $detalle ){       
 
-   
+        if(empty($producto)) {
+             throw new Exception("::Error, debe indicar al menos  el nombre del producto para esta operación ");
+        }
+        try{
+            if($id_producto != 0){
+                // llegó id valido que exista para hacer update
+                $selectStat = "SELECT * FROM producto WHERE id_producto = $id_producto ";
+                $resultado = $this->ResultQuery($selectStat);
+                if(count($resultado) > 0){                
+                    // existe el id en la tabla
+                    $updateStat = "update producto set ";
+                    $updateStat .= "producto ='$producto',";
+                    $updateStat .= "detalle = $detalle',";
+                    
+                    $this->NoResultQuery($updateStat);
+                }else{
+                    // no existe el id en la tabla
+                    throw new Exception("::El producto con id $id_producto no existe ");
+                } 
+            }else{
+                // hago el insert
+                $insertStat = "INSERT INTO producto (producto,detalle,codigo_producto) VALUES(";
+                $insertStat .= "'$producto','$detalle',";
+                $insertStat .= " $codigo_producto)";
+                $this->NoResultQuery($insertStat);
+            }
+        }catch (Exception $e){
+            throw new Exception("::Error obteniendo producto  ".$e->getMessage());         
+        }
+    }
+   public function delete($id_producto){          
+        try{            
+
+            $recproducto =  $this->getByid($id_producto);
+
+            $selectStat = "select id_producto from producto where id_producto = $id_producto";
+            $resultado = $this->ResultQuery($selectStat);
+            if(count( $resultado)==0){
+
+                $this->iniciarTransaccion();
+                
+                $this->NoResultQuery("DELETE FROM producto WHERE id_producto = $id_producto ");
+                   
+            }else{
+                throw new Exception(" No puede eliminarse el producto");
+            }
+        }   
+            
+    }
    
 }
 ?>
