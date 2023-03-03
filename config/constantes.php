2cbbfc42a8bd621823b164e41100cc5d98a67a85
Sebastian Lanzetti <90937504+SebaLanz@users.noreply.github.com>
2023-03-03 12:55:16 -0300

    Update constantes.php

--- config/constantes.php
+++ config/constantes.php
@@ -1,13 +1,13 @@
 <?php
-	define("DBhost",'localhost'); // define la constante DBhost con el valor 'localhost'
-	define("DBuser",'root'); // define la constante DBuser con el valor 'root'
+	define("DBhost",''); // define la constante DBhost con el valor 'localhost'
+	define("DBuser",''); // define la constante DBuser con el valor 'root'
 	define("DBpass",'') ; // define la constante DBpass con el valor ‘’	
-	define("DBbase",'seguro'); // define la constante DBbase con el valor 'seguros'
-	define("DBpuerto",3306); // define la constante DBpuerto con el valor 3306
-	define("CONFIG_API_KEY",'proy2022');// key de encriptación para tokens
+	define("DBbase",''); // define la constante DBbase con el valor 'seguros'
+	define("DBpuerto",); // define la constante DBpuerto con el valor 3306
+	define("CONFIG_API_KEY",'');// key de encriptación para tokens
 	// constantes del servicio de envio de mails	
-	define("SMTP_SERVER",'smtp.gmail.com');// host smtp
-	define("SMTP_USER",'sbjjnz@gmail.com');//usuario con acceso al smtp 
-	define("SMTP_PASSWORD",'sebitah37');// pass del servidor smtp
-	define("SMTP_PORT",587);// puerto del servidor smtp
+	define("SMTP_SERVER",'');// host smtp
+	define("SMTP_USER",');//usuario con acceso al smtp 
+	define("SMTP_PASSWORD",'');// pass del servidor smtp
+	define("SMTP_PORT",);// puerto del servidor smtp
 ?>
