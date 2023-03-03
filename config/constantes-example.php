da6e097ae0f0442c92b60a65991fbb23656a4d76
Sebastian Lanzetti <90937504+SebaLanz@users.noreply.github.com>
2023-03-03 12:57:50 -0300

    Create constantes-example.php

---
+++ config/constantes-example.php
@@ -0,0 +1,12 @@
+<?php
+	define("DBhost",''); 
+	define("DBuser",''); 
+	define("DBpass",'') ; 
+	define("DBbase",''); 
+	define("DBpuerto",); 
+	define("CONFIG_API_KEY",'');
+	define("SMTP_SERVER",'');
+	define("SMTP_USER",''); 
+	define("SMTP_PASSWORD",'');
+	define("SMTP_PORT",);
+?>
