ffeb8e1455b2eb890cefd32560074b1cac0e805a
Sebastian Lanzetti <90937504+SebaLanz@users.noreply.github.com>
2023-03-03 12:58:24 -0300

    Update constantes.php

--- config/constantes.php
+++ config/constantes.php
@@ -1,13 +1,13 @@
 <?php
-	define("DBhost",''); // define la constante DBhost con el valor 'localhost'
-	define("DBuser",''); // define la constante DBuser con el valor 'root'
-	define("DBpass",'') ; // define la constante DBpass con el valor ‘’	
-	define("DBbase",''); // define la constante DBbase con el valor 'seguros'
-	define("DBpuerto",); // define la constante DBpuerto con el valor 3306
-	define("CONFIG_API_KEY",'');// key de encriptación para tokens
-	// constantes del servicio de envio de mails	
-	define("SMTP_SERVER",'');// host smtp
-	define("SMTP_USER",');//usuario con acceso al smtp 
-	define("SMTP_PASSWORD",'');// pass del servidor smtp
-	define("SMTP_PORT",);// puerto del servidor smtp
+	define("DBhost",''); // 
+	define("DBuser",''); // 
+	define("DBpass",'') ; // 
+	define("DBbase",''); // 
+	define("DBpuerto",); //
+	define("CONFIG_API_KEY",'');
+	
+	define("SMTP_SERVER",'');
+	define("SMTP_USER",');
+	define("SMTP_PASSWORD",'');
+	define("SMTP_PORT",);
 ?>
